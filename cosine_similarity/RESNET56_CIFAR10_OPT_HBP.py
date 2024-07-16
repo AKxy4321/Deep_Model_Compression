3a71aa994a1e452eae732f298b0575d9ef924ab0
AKxy4321 <adityakushal23@gmail.com>
2024-07-17 00:55:56 +0530

    updated resent56

--- cosine_similarity/RESNET56_CIFAR10_OPT_HBP.py
+++ cosine_similarity/RESNET56_CIFAR10_OPT_HBP.py
@@ -703,7 +703,7 @@ model.compile(loss='categorical_crossentropy',
 
 
 # Prepare model model saving directory.
-save_dir = os.path.join('.', 'saved_models')
+save_dir = os.path.join('.', 'models')
 model_name = 'cifar10_%s_model.{epoch:03d}.h5' % model_type
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
