7f273799b7fb1e147fff119527607f29ea03b976
AKxy4321 <adityakushal23@gmail.com>
2024-07-22 19:36:03 +0530

    removed buggy lines in lenet5

--- cosine_similarity/lenet5_MNIST_OPT_HBP_v2.py
+++ cosine_similarity/lenet5_MNIST_OPT_HBP_v2.py
@@ -237,10 +237,6 @@ def my_get_filter_pruning_indices(episodes_for_all_layers,l1_norm_matrix_list):
         a = list(a)
         filter_pruning_indices.append(a)
 
-    output_path = os.path.join('cosine_similarity', 'results', 'cosine_pruned_filters.txt')
-    with open(output_path, 'a') as f:
-        for indices in filter_pruning_indices:
-            f.write(' '.join(map(str, indices)) + '\n')
     return filter_pruning_indices
 
 
