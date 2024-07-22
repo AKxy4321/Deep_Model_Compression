b4884be6e79f568144545f647505bd5598b7df0b
AKxy4321 <adityakushal23@gmail.com>
2024-07-22 20:09:26 +0530

    updated hyperparameters lenet5

--- cosine_similarity/lenet5_MNIST_OPT_HBP_v2.py
+++ cosine_similarity/lenet5_MNIST_OPT_HBP_v2.py
@@ -603,14 +603,14 @@ while validation_accuracy - max_val_acc >= -0.01:
         
 
     if count < 1:
-        optimize(model,weight_list_per_epoch,10,50,True)
-        model = my_delete_filters(model,weight_list_per_epoch,50,True)
+        optimize(model,weight_list_per_epoch,10,30,True)
+        model = my_delete_filters(model,weight_list_per_epoch,30,True)
         model,history,weight_list_per_epoch = train(model,10,False)
    
     else:
-        optimize(model,weight_list_per_epoch,10,30,False)
-        model = my_delete_filters(model,weight_list_per_epoch,30,False)
-        model,history,weight_list_per_epoch = train(model,20,False)
+        optimize(model,weight_list_per_epoch,10,15,False)
+        model = my_delete_filters(model,weight_list_per_epoch,15,False)
+        model,history,weight_list_per_epoch = train(model,10,False)
 
     a,b,c,d = count_model_params_flops(model,False)
     print(a,b,c,d)
